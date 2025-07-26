bf5770e5f31d8cd28013a118a0671294cf75f73d
Brice Patchou <108960259+Brice12347@users.noreply.github.com>
2025-07-25 22:28:53 -0700

    changed resume and about page

--- one-portfolio/src/components/About.jsx
+++ one-portfolio/src/components/About.jsx
@@ -11,13 +11,13 @@ const About = () => {
                     </p>
                 </div>
                 <p className='text-xl mt-20'>
-                    Hello, my name is Brice Patchou. I am currently a Junior studying Computer Science and Business Administration. One important thing you should know about me is that I am the type of person who loves to optimize my life using computer science. I enjoy learning new ways to improve my skills and simplify repetitive tasks. Recently, I learned how to use the FPDF library to create PDF cover letters automatically, which saved me a lot of time. My ultimate career goal is to use my skills to contribute to a product that can positively impact the world.
+                Hello, my name is Brice Patchou. I am a senior studying Computer Science and Business Administration. I’m passionate about using technology to make a positive impact on those around me. As a Software Engineering Intern at Boeing, I developed unit tests in C# and authored a comprehensive guide to improve collaboration and code reliability across teams—an experience that taught me the value of scalable, well-documented solutions. My ultimate goal is to contribute to projects that drive meaningful change and empower the people around me.
                 </p>
 
                 <br />
 
                 <p className='text-xl'>
-                    I absolutely adore hanging out with my friends and family whenever I'm not working on my side project. I'm a big fan of playing basketball with my buddies and enjoying a game of Monopoly with my siblings. I also love hitting the gym regularly and pumping some iron. It's always more fun with a friend, but I don't mind working out alone either!
+                    I absolutely adore hanging out with my friends and family whenever I'm not working on my side project. I'm a big fan of playing basketball with my friends and enjoying a game of Monopoly with my siblings. I also love hitting the gym regularly and pumping some iron. It's always more fun with a friend, but I don't mind working out alone either!
                 </p>
             </div>
         </div>
